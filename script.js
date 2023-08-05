3311128a7064ec07159c90b5c9b3862ab85ae158
BagaturBey <bahadir_by@hotmail.com>
2023-08-06 01:40:28 +0300

    Mail-Kontrol

---
+++ script.js
@@ -0,0 +1,21 @@
+const emailInput = document.getElementById('emailInput');
+const submitButton = document.getElementById('submitButton');
+const result = document.getElementById('result');
+
+emailInput.addEventListener('input', function () {
+    if (isValidEmail(emailInput.value)) {
+        submitButton.removeAttribute('disabled');
+    } else {
+        submitButton.setAttribute('disabled', 'true');
+    }
+});
+
+submitButton.addEventListener('click', function () {
+    if (isValidEmail(emailInput.value)) {
+        result.textContent = 'Bu bir mail adresi: ' + emailInput.value;
+    }
+});
+
+function isValidEmail(email) {
+    return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
+}
